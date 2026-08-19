989ec8be0f5a87366974f599873c24d516f20b43
roberto <jrobert@gmail.com>
2026-08-19 02:04:42 +0200

    creacion de metodo de pares e impares

--- src/prueeee/utilitarios.java
+++ src/prueeee/utilitarios.java
@@ -1,10 +1,16 @@
-
 package prueeee;
 
 public class utilitarios {
-    public  void imprimir(int []num){
+
+    public void imprimir(int[] num) {
         for (int i = 0; i < num.length; i++) {
-            System.out.println("El valor"+(i+1)+"es: "+num[i]);                
+            System.out.println("El valor" + (i + 1) + "es: " + num[i]);
         }
-}
+    }
+    public  void pares(int []par, int cont, int valor){
+        par[cont]=valor;
+    }
+    public  void impares(int []par, int cont, int valor){
+        par[cont]=valor;
+    }
 }
